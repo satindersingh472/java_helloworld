232dcacac5f51b5f8dc1a466a21a8252b0a6f96c
satinder <satindersingh472@gmail.com>
2022-12-11 09:24:09 -0700

    inputs_scanner

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-    String lyric = "roses are red, violets are blue";
-    if (lyric.contains("blue")){
-        System.out.println("The lyrics has the word blue.");
-    } else {
-        System.out.println("The lyrics don't have the word blue");
-    }
+    Scanner scanner = new Scanner(System.in);
 
+    System.out.print("What is your name?");
+    String name = scanner.nextLine();
+
+    System.out.printf("So %s. How old are you?",name);
+    int age = scanner.nextInt();
+
+    System.out.printf("Cool %d is a good age to start programming", age);
+
+    scanner.close();
     }
 }
