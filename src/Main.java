7a5d6970c0d7a792a42abf24b0b6212494020877
satinder <satindersingh472@gmail.com>
2022-12-11 09:47:28 -0700

    switch_statements

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,49 @@ public class Main {
     System.out.printf("So %s. How old are you?",name);
     int age = scanner.nextInt();
 
-    System.out.printf("Cool %d is a good age to start programming", age);
+    scanner.nextLine();
+    System.out.printf("Cool %d is a good age to start programming. \n what language do you prefer?", age);
+    String language = scanner.nextLine();
+    System.out.printf("Ah!! %s is a solid programming language." , language);
+    System.out.println("What is the first number?");
+//    will take the input as an integer
+    int a = scanner.nextInt();
+//    will consume the next dangling newline character
+    scanner.nextLine();
+
+//    ask for the second input
+    System.out.print("What is the second number");
+    int b = scanner.nextInt();
+
+    scanner.nextLine();
+
+    System.out.print("What operation would you like to perform?");
+    System.out.print("subtract, sum, multiply, division");
+    String operation = scanner.nextLine();
+
+    switch (operation){
+        case "sum":
+            System.out.printf("%d + %d = %d" ,a,b,a+b);
+            break;
+        case "subtract":
+            System.out.printf("%d - %d = %d" , a,b,a-b);
+            break;
+        case "multiply":
+            System.out.printf("%d * %d = %d" , a, b , a*b);
+            break;
+        case "division":
+            if (b == 0){
+                System.out.print("Can not divide by 0");
+            } else {
+                System.out.printf("%d / %d = %d" , a,b,a/b);
+                break;
+            }
+        default:
+            System.out.printf("Invalid Operation");
+    }
+    scanner.close();
+
+
 
     scanner.close();
     }
