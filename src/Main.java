65fdf86a98d93bfad5ad815e5677afbad0256c78
satinder <satindersingh472@gmail.com>
2022-12-14 17:09:09 -0700

    working_with_loo[ps

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
+
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args){
-    int random_numbers[] = {1,2,10,5,15,20,4,8,6};
-    for (int number : random_numbers){
-        if (number % 5 == 0 ) {
-            System.out.println(number);
-        } else {
-              System.out.println(String.format(number + " is not divisible by 5"));
+        int[] random_values = {1,2,3,4};
+        for (int number = 0; number <= random_values.length;number++){
+            if (random_values[number] % 2 == 1){
+                System.out.println(String.format("%d is a odd number ", random_values[number] ));
+            } else {
+                System.out.println(random_values[number]);
             }
-
+        }
+        }
     }
-
-
-    }
-}
